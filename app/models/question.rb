dfdccb20d1459d8f38939f40c912eeac0e97966b
Will Ashley <gameprobot23@yahoo.com>
2016-07-28 14:00:50 -0700

    Adds question#results to display number of times a answer choice was chosen for a set of responses to a question.

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,5 +15,28 @@ class Question < ActiveRecord::Base
     through: :answer_choices,
     source: :responses
 
-
+    # SELECT
+    #   answers.id, COUNT(responses)
+    # FROM
+    #   questions
+    # JOIN
+    #   answers ON answers.question_id = questions.id
+    # LEFT OUTER JOIN
+    #   responses ON responses.answer_id = answers.id
+    # WHERE
+    #   question_id = 1
+    # GROUP BY
+    #   answers.id
+  def results
+    ans_hash = Hash.new(0)
+    h = self
+      .answer_choices
+      .select("answers.body, COUNT(responses.id) AS response_count")
+      .joins('left outer join responses on responses.answer_id = answers.id')
+      .group("answers.id")
+    h.map do |ans|
+      ans_hash[ans.body] = ans.response_count
+    end
+    ans_hash
+  end
 end
